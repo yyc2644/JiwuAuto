5f0f7b8342cad0161456d02c6fcde9812d385acd
YiCheng <yichengace@hotmail.com>
2017-06-12 18:11:50 +0800

    111

--- JW/Traversal City.py
+++ JW/Traversal City.py
@@ -1,21 +1,30 @@
 #-*- coding: UTF-8 -*-
 #遍历所有城市，目的是为了获得城市，然后输出list，为后续自动化做准备
 #原理：利用正则表达式来匹配符合要求的地址
-from selenium import webdriver
+import urllib.request
 import re
 
-driver =  webdriver.Chrome("/Applications/chromedriver")
-driver.get("http://www.jiwu.com/")
-page = driver.page_source
-# print(page)
+url = "http://www.jiwu.com"
+# 输出网址，输出页面代码
+def get_WebPage(url):
+    WebPageUrl = urllib.request.urlopen(url)
+    WebPage = WebPageUrl.read().decode("utf8")
+    WebPageUrl.close()
+    # print(WebPage)
+    return WebPage
 
 
-#非贪婪模式，re.s（'.'匹配字符，包括换行）
-city_list = re.findall('[a-zA-z]+://[^\s]*com',page,re.S)
-re_rule = "(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?"
-# city_list = re.findall(re_rule,page)
+# get_WebPage(url)
+
+
+# 匹配所有jiwu.com结尾的网址，数据来源 上一个函数，非贪婪模式
+city_list = re.findall('http://[^\s]*jiwu.com',get_WebPage(url),re.S)
+
+#数据去重
+city_list = list(set(city_list))
+
+
+print(city_list)
 # print(city_list)
-for clist in  city_list:
-    print(clist)
 
-# print(city_list)
+# help(re)
--- JW/__init__.py
+++ JW/__init__.py
@@ -1,12 +1,1 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome("/Applications/chromedriver")
-browser.get("http://bj.jiwu.com/loupan/54410.html")
-
-browser.find_element_by_id("mobile520").send_keys("13691712693")
-browser.find_element_by_id("yzm520").send_keys("470515")
-
-
-
-if 1==1:
-    browser.find_element_by_class_name("getyzm").click()
+#-*- coding: UTF-8 -*-
