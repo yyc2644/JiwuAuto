fc99fca3d7f1f43cf36ebe4042e5ce3eab1531a6
YiCheng <yichengace@hotmail.com>
2017-06-13 20:27:48 +0800

    0613
    add some code

--- JW/City_Home.py
+++ JW/City_Home.py
@@ -1,4 +1,7 @@
 #-*- coding: UTF-8 -*-
+#示例代码，先以一个城市为原型抓去数据
+
+#-*- coding: UTF-8 -*-
 #遍历所有城市，目的是为了获得城市，然后输出list，为后续自动化做准备
 #原理：利用正则表达式来匹配符合要求的地址
 import urllib.request
@@ -13,20 +16,19 @@ def get_WebPage(url):
     WebPageUrl = urllib.request.urlopen(url)
     WebPage = WebPageUrl.read().decode("utf8")
     WebPageUrl.close()
-    # print(WebPage)
     return WebPage
 #将得到的数据变更成适合存入数据库的样式
 def Forma_change():
 #匹配所有jiwu.com结尾的网址，数据来源 上一个函数，非贪婪模式
-    city_list = re.findall('http://[^\s]*jiwu.com',get_WebPage(url),re.S)
+    city_list = re.findall('http://\w*.\w*.\w*.\w*[^a-zA-Z0-9]',get_WebPage(url))
 #数据去重
     city_list = list(set(city_list))
-
-    print(city_list)
 
     return city_list
 
 
 
 if __name__ == "__main__":
-    Forma_change()
+
+    print(Forma_change())
+
