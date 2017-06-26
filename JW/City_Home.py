ba977322cd37123064f658ccf79583faa280c2a1
YiCheng <yichengace@hotmail.com>
2017-06-26 18:30:51 +0800

    beautiful suop start

--- JW/City_Home.py
+++ JW/City_Home.py
@@ -1,40 +1,15 @@
 #-*- coding: UTF-8 -*-
 #示例代码，先以一个城市为原型抓去数据
 
-#-*- coding: UTF-8 -*-
 #遍历所有城市，目的是为了获得城市，然后输出list，为后续自动化做准备
 #原理：利用正则表达式来匹配符合要求的地址
 import urllib.request
-import re
+from bs4 import BeautifulSoup
+
 
 #未来改进的时候，多进程可能会用到Queue
-
 '''
-废弃代码
-'''
-# def get_WebPage(url):
-#     WebPageUrl = urllib.request.urlopen(url)
-#     WebPage = WebPageUrl.read().decode("utf8")
-#     WebPageUrl.close()
-#     return WebPage
-#将得到的数据变更成适合存入数据库的样式
-# def Forma_change():
-# #匹配所有jiwu.com结尾的网址，数据来源 上一个函数，非贪婪模式
-#
-#     city_list = re.findall('http://\w*.\w*.\w*.\w*',get_WebPage(url))
-# #数据去重
-#     city_list = list(set(city_list))
-#
-#     return city_list
-
-from selenium import webdriver
-
-# driver = webdriver.Chrome("/Applications/chromedriver")
-# driver.get("http://cs.jiwu.com")
-# page = driver.page_source
-# print page
-
-#打开网页
+#re不会用，先拿beautifulsoup凑数
 url = "http://cs.jiwu.com"
 
 WebPageUrl = urllib.request.urlopen(url)
@@ -52,8 +27,10 @@ for url in url_list:
         print(url)
         url_all.append(url)
 
+'''
 
-if __name__ == "__main__":
 
-    print("-------",url_all,"--------------",url_list)
+
+# if __name__ == "__main__":
+
 
--- JW/PageAnalysis.py
+++ JW/PageAnalysis.py
@@ -1,5 +1,6 @@
 #-*- coding: UTF-8 -*-
 # coding: UTF-8
+#demo：https://blog.imdst.com/pythonzhong-yong-pycurljian-kong-httpxiang-ying-shi-jian-jiao-ben/
 
 from io import StringIO
 import pycurl
@@ -8,6 +9,7 @@ import os
 import json
 
 class Http_Test():
+    #直接拷贝别人家的代码，不知道这个类有什么用
     def __init__(self):
         self.contents = ""
     def body_callback(self,buf):
@@ -18,30 +20,37 @@ class Http_Test():
 
 
 def Check_Url(input_url):
+    #基本设置
     t = Http_Test()
     c = pycurl.Curl()
     c.setopt(pycurl.WRITEFUNCTION,t.body_callback)
     c.setopt(pycurl.ENCODING, 'gzip')
     c.setopt(pycurl.URL,input_url)
     c.perform()
-    http_code = c.getinfo(pycurl.HTTP_CODE)
-    http_conn_time = c.getinfo(pycurl.CONNECT_TIME)
-    http_pre_tran = c.getinfo(pycurl.PRETRANSFER_TIME)
-    http_start_tran = c.getinfo(pycurl.STARTTRANSFER_TIME)
-    http_total_time = c.getinfo(pycurl.TOTAL_TIME)
-    http_size = c.getinfo(pycurl.SIZE_DOWNLOAD)
-    http_namelookup_time = c.getinfo(pycurl.NAMELOOKUP_TIME)
-    http_connectcode = c.getinfo(pycurl.HTTP_CONNECTCODE)
+    #定义需要监控的数据，目前只需要响应代码和链接时间
+    http_code = c.getinfo(pycurl.HTTP_CODE)                             #响应代码
+    http_conn_time = c.getinfo(pycurl.CONNECT_TIME)                     #连接时间
+    http_pre_tran = c.getinfo(pycurl.PRETRANSFER_TIME)                  #连接上后到开始传输时间
+    http_start_tran = c.getinfo(pycurl.STARTTRANSFER_TIME)              #接收到第一个字节的时间
+    http_total_time = c.getinfo(pycurl.TOTAL_TIME)                      #域名解析时间
+    http_size = c.getinfo(pycurl.SIZE_DOWNLOAD)                         #请求总的时间
+    http_namelookup_time = c.getinfo(pycurl.NAMELOOKUP_TIME)            #域名解析时间
+    http_connectcode = c.getinfo(pycurl.HTTP_CONNECTCODE)               #连接代码
+    #输出模版
     info = """
-    监测URL：%s
-    响应代码: %s
-    连接代码: %s
-    连接时间: %.3f秒
-    域名解析时间: %.3f秒
-    请求总的时间: %.3f秒
-    连接上后到开始传输时间: %.3f秒
-    接收到第一个字节的时间: %.3f秒
-    """ %( input_url,http_code,http_connectcode,http_conn_time,http_namelookup_time,http_total_time,http_pre_tran,http_start_tran)
+            监测URL：%s
+            响应代码: %s
+            连接时间: %.3f秒
+            域名解析时间: %.3f秒
+            请求总的时间: %.3f秒
+    """ \
+           %(
+            input_url,
+            http_code,
+            http_conn_time,
+            http_namelookup_time,
+            http_total_time
+           )
     return info
 
 if __name__ == "__main__":
